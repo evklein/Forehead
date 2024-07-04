8447548f2b4a4cc59e63f7bdd9368512fcb0e17e
hasherr <eklein217@gmail.com>
2024-07-04 13:25:14 -0400

    Attempting to get static files working

--- api/bonk/urls.py
+++ api/bonk/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path("course/", include("course.urls")),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
