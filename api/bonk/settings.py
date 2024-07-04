538b1b0f4db225abb3f7c6a42b087ac25ade9c6d
hasherr <eklein217@gmail.com>
2024-07-03 22:23:12 -0400

    Adding allowed host for azure

--- api/bonk/settings.py
+++ api/bonk/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-wyydqsp9kmp15(5ypkrat0n^fa#whn37xk^c_ohf+=_lu5-bm4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # Application definition
 
